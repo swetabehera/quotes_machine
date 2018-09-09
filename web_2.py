f8a7f76f66e7f84aa658de66bd929818c6bcd79f
Sweta Behera <sweta.saena@gmail.com>
2018-09-09 12:54:24 +0530

    Add game feature

--- web_2.py
+++ web_2.py
@@ -19,7 +19,41 @@ so=BeautifulSoup(re.text,"html.parser")
 first_letter=author[0]
 last_letter=author.split()[-1][0]
 born=so.find(class_="author-born-date").get_text()+" "+so.find(class_="author-born-location").get_text()
-print(quote,author,born,first_letter,last_letter)
+
+
+sweta=True
+while(sweta):
+	print("Here's a quote:\n")
+	print(quote)
+	x=4
+	answer=input("\nWho said this? Guesses remaining "+str(x)+"\n")
+	if answer ==  author:
+		print("You guessed correctly! Congratulations !!!")
+	else:
+		x=x-1
+		print("Here's a hint. The author was born on "+ born)
+		answer=input("\nWho said this? Guesses remaining "+str(x)+"\n")
+		if answer ==author:
+			print("You guessed correctly! Congratulations !!!")
+		else:
+			x=x-1
+			print("Here's a hint. The author's first name starts with "+ first_letter)
+			answer=input("\nWho said this? Guesses remaining "+str(x)+"\n")
+			if answer ==author:
+				print("You guessed correctly! Congratulations !!!")
+			else:
+				x=x-1
+				print("Here's a hint. The author's last name starts with "+ last_letter)
+				answer=input("\nWho said this? Guesses remaining "+str(x)+"\n")
+				if answer ==author:
+					print("You guessed correctly! Congratulations !!!")
+				else: 
+					print("Sorry, You have run out of Guesses. The answer is"+author)
+	ch=input("Would you like to play again?(y/n)\n")
+	if ch=="n":
+		sweta=False
+
+
 
 #with open("web_scr.csv","w") as file:
 #	csv_writer=writer(file)
